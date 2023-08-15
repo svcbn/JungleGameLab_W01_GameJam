74755c98422658709069a507d1bb2d93ea3f5988
MeganTrioMusic <coomott@naver.com>
2023-08-15 15:24:52 +0900

    EnemyController, SpawnManager

--- Assets/HK/EnemyController.cs
+++ Assets/HK/EnemyController.cs
@@ -1,18 +1,47 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float moveSpeed = 3.0f; // 이동 속도
+
+    private GameObject player; // 플레이어 오브젝트
+
+    private void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player");      
     }
 
-    // Update is called once per frame
-    void Update()
+
+    //플레이어와의 충돌시, 데미지 메서드 호출. 재 확인 필요
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
+        if (other.CompareTag("Player"))
+        {
+            PlayerController playerController = other.GetComponent<PlayerController>();
+            if (playerController != null)
+            {
+                playerController.GetDamage(); 
+            }           
+        }
     }
+
+    //코루틴으로 플레이어 추격
+    public IEnumerator ChasePlayer(Transform enemyTransform)
+    {
+        while (true)
+        {
+            if (player != null && enemyTransform != null)
+            {
+                Vector2 targetPosition = player.transform.position;
+                Vector2 newPosition = Vector2.MoveTowards(enemyTransform.position, targetPosition, moveSpeed * Time.deltaTime);
+                enemyTransform.position = newPosition;
+            }
+
+            yield return null;
+        }
+    }
+
 }
+
+
