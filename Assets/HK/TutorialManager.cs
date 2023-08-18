2b2faa2a20f67627e5126b7e678e3000c9d1f055
MeganTrioMusic <coomott@naver.com>
2023-08-18 20:22:58 +0900

    Tutorial Modify + Map

--- Assets/HK/TutorialManager.cs
+++ Assets/HK/TutorialManager.cs
@@ -2,18 +2,20 @@ using UnityEngine;
 using UnityEngine.UI;
 using static UnityEngine.UI.GraphicRaycaster;
 
+//HK 
 public class TutorialManager : MonoBehaviour
 {
     public static TutorialManager instance;
     
-    public GameObject playerPrefab;
-    public Vector2 fixedPlayerPosition;
-    public GameObject[] tutorialObjects; 
-    public string[] soundEvent;
-    public Button tutorialButton;
+    
+    public GameObject playerPrefab; // 플레이어 프리팹 -> 17번 튜토리얼때 스폰 됨
+    public Vector2 fixedPlayerPosition; // 플레이어 스폰 고정 위치
+    public GameObject[] tutorialObjects; // 튜토리얼에 사용될 오브젝트 리스트
+    public Button tutorialButton; //튜토리얼 스텝을 트리거 할 버튼 오브젝트
     private int currentStep = 0;
 
     private int[] sensorStepMapping = { 20, 21, 22, 19, 23, 24, 25};
+    //센서가 총 7개 존재 한다는 뜻. 센서에 인덱스가 붙어있으며, 센서와 트리거 되는 Step의 순서가 1:1 로 부착 되어있다는 뜻!
 
     private void Awake()
     {
@@ -42,6 +44,7 @@ public class TutorialManager : MonoBehaviour
 
     private void ShowStep(int stepIndex)
     {
+        //튜토리얼 종료 후 데이 2로 넘어간다.
         if (stepIndex >= 25)
         {
             UIManager.instance.Tutorial_Reset();
@@ -51,25 +54,27 @@ public class TutorialManager : MonoBehaviour
         
         if (stepIndex >= 0 && stepIndex < tutorialObjects.Length)
         {
+            // 기본 출력 함수
             for (int i = 0; i < tutorialObjects.Length; i++)
             {
                 tutorialObjects[i].SetActive(i == stepIndex);
             }
 
+            //(특수 스텝)1 ~ 8 스텝 까지 상점 UI를 유지 하는 기능
             if ((stepIndex >= 1 && stepIndex <= 8))
             {
                 tutorialObjects[1].SetActive(true);
-                Debug.Log("Previous Step - stepIndex: 1");
                 UIManager.instance.Tutorial_Shop();
 
             }
 
+            //(특수 스텝) 12 ~ 15 스텝 동안 낮 맵을 유지
             if ((stepIndex >= 12 && stepIndex <= 15))
             {
                 tutorialObjects[12].SetActive(true);
-                Debug.Log("Previous Step - stepIndex: 1");
             }
 
+            //(특수스텝) 17 스텝(밤 맵 등장)에 플레이어를 스폰 하고 밤 전용 카메라 크기를 적용 한다.
             if (stepIndex == 17)
             {
                 
@@ -80,14 +85,18 @@ public class TutorialManager : MonoBehaviour
                 Camera.main.orthographicSize /= 4.0f;
             }
 
+            //(특수스텝) 17 ~ 26 스텝 동안 밤 맵을 유지
             if ((stepIndex >= 17 && stepIndex <= 26))
             {
-                tutorialObjects[17].SetActive(true); 
-                
-                if(stepIndex == 17)  
-                    tutorialButton.gameObject.SetActive(false);
+                tutorialObjects[17].SetActive(true);                      
             }
 
+            //(특수스텝) 17 스텝(밤 맵) 이 되면 튜토리얼 버튼을 비활성화 한다.
+            if (stepIndex == 17)
+            {
+                tutorialButton.gameObject.SetActive(false);
+            }
+            //(마지막 특수스텝) 9 ~ 11 스텝 동안 나침반 오브젝트를 유지 한다.        
             else if ((stepIndex >= 9 && stepIndex <= 11))
             {
                 UIManager.instance.Tutorial_Game();
@@ -107,29 +116,14 @@ public class TutorialManager : MonoBehaviour
 
     private void HideStep(int stepIndex)
     {
-        if (stepIndex != 1 && stepIndex != 8)
+        //1, 8, 12, 15 일때 빼고 남은 스텝들은 다음 스텝이 되는 즉시 사라진다!
+        if (stepIndex != 1 && stepIndex != 8 && stepIndex != 12 && stepIndex != 15)
         {
             tutorialObjects[stepIndex].SetActive(false);
-        }
-
-        if (stepIndex != 12 && stepIndex != 15)
-        {
-            tutorialObjects[stepIndex].SetActive(false);
-        }
-
-        if (stepIndex == 8)
-        {
-            tutorialObjects[1].SetActive(false);
-            tutorialObjects[8].SetActive(false);
-        }
-
-        else if (stepIndex == 16)
-        {
-            tutorialObjects[12].SetActive(false);
-            tutorialObjects[15].SetActive(false);
         }
     }
 
+    //위의 센서 매핑에 부여한 타겟 스텝 을 부착 해준다!
     public void GoToSensorStep(int sensorID)
     {
         if (sensorID >= 0 && sensorID < sensorStepMapping.Length)
@@ -137,16 +131,6 @@ public class TutorialManager : MonoBehaviour
             int targetStep = sensorStepMapping[sensorID];
             HideStep(currentStep);
             ShowStep(targetStep);
-        }
-    }
-
-    private void ChangeButtonColor(Button button, Color color)
-    {
-        if (button != null && button.gameObject.activeSelf)
-        {
-            ColorBlock colorBlock = button.colors;
-            colorBlock.normalColor = color;
-            button.colors = colorBlock;
         }
     }
 
